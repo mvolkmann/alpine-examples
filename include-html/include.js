77639602a3930837dc8a711a77eecea64c254e03
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-14 10:21:32 -0600

    progress

--- include-html/include.js
+++ include-html/include.js
@@ -3,20 +3,21 @@ const lastSlashIndex = href.lastIndexOf("/");
 const urlPrefix = href.substring(0, lastSlashIndex + 1);
 
 function includeHTML() {
+  const attribute = "x-include";
   // Find the first element that contains the include attribute.
-  const element = document.querySelector("[include");
+  const element = document.querySelector(`[${attribute}]`);
   if (!element) return; // no more found
 
   const xhr = new XMLHttpRequest();
   xhr.onload = () => {
     element.innerHTML = xhr.responseText;
-    element.removeAttribute("include");
+    element.removeAttribute(attribute);
     // Make a recursive call to process remaining elements
     // with the w3-include-html attribute.
     includeHTML();
   };
 
-  const file = element.getAttribute("include");
+  const file = element.getAttribute(attribute);
   xhr.open("GET", urlPrefix + file, true);
   xhr.send();
 }
