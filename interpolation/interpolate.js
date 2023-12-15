6011de48632e3ae1a148d832f416864eb7d8596b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-15 08:48:47 -0600

    progress

--- interpolation/interpolate.js
+++ interpolation/interpolate.js
@@ -1,13 +1,28 @@
 // TODO: Turn this into an official Alpine plugin.
-const RE = /{([^}]+)}/g;
+let defaultDelimiters = "{}";
+let re;
+
+function getRegExp(delimiters) {
+  let len = delimiters.length;
+  if (len % 2 !== 0) {
+    throw new Error(`x-interp invalid delimiters: ${delimiters}`);
+  }
+  len /= 2;
+  const start = delimiters.slice(0, len);
+  const end = delimiters.slice(len);
+  return new RegExp(`${start}([^}]+)${end}`, "g");
+}
 
 function updateText(el, evaluate) {
   for (const child of el.childNodes) {
     const { nodeType } = child;
     if (nodeType === Node.TEXT_NODE) {
       if (!child.originalValue) child.originalValue = child.nodeValue;
-      child.nodeValue = child.originalValue.replaceAll(RE, (_, capture) =>
-        evaluate(capture)
+      child.nodeValue = child.originalValue.replaceAll(
+        re,
+        // evaluate doesn't throw an error if the expression is invalid.
+        // It outputs an error message and returns undefined.
+        (_, capture) => evaluate(capture) || capture
       );
     } else if (nodeType === Node.ELEMENT_NODE) {
       updateText(child, evaluate); // recursive call
@@ -18,7 +33,8 @@ function updateText(el, evaluate) {
 document.addEventListener("alpine:init", () => {
   // TODO: Pass to this the delimiters to use, ex. '{{}}' or '{}'.
   // Have default delimiters, allow the default to be overridden, and allow per-use delimiters.
-  Alpine.directive("interp", (el, {}, { effect, evaluate }) => {
+  Alpine.directive("interp", (el, { expression }, { effect, evaluate }) => {
+    re = getRegExp(expression || defaultDelimiters);
     effect(() => updateText(el, evaluate));
   });
 });
