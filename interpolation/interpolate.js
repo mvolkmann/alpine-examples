bb3e94dd290b2aefdb7751a28a683637ad440d58
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-14 19:29:25 -0600

    progress

--- interpolation/interpolate.js
+++ interpolation/interpolate.js
@@ -2,17 +2,13 @@
 const RE = /{([^}]+)}/g;
 
 function updateText(el, evaluate) {
-  console.log("interpolate.js updateText: entered");
   for (const child of el.childNodes) {
     const { nodeType } = child;
     if (nodeType === Node.TEXT_NODE) {
-      const text = child.textContent;
-      // TODO: How can you use a capture group to get only inside the braces?
-      const newText = text.replaceAll(RE, (match) => {
-        const expression = match.substring(1, match.length - 1);
-        return evaluate(expression);
-      });
-      child.nodeValue = newText;
+      if (!child.originalValue) child.originalValue = child.nodeValue;
+      child.nodeValue = child.originalValue.replaceAll(RE, (_, capture) =>
+        evaluate(capture)
+      );
     } else if (nodeType === Node.ELEMENT_NODE) {
       updateText(child, evaluate); // recursive call
     }
