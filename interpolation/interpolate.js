f2d595cad3c62948047abc10d5e36869b240db6b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-14 18:31:54 -0600

    progress

--- interpolation/interpolate.js
+++ interpolation/interpolate.js
@@ -1,27 +1,24 @@
-const { href } = location;
-const lastSlashIndex = href.lastIndexOf("/");
-const urlPrefix = href.substring(0, lastSlashIndex + 1);
+const RE = /{([^}]+)}/g;
 
 function updateText(el, evaluate) {
   for (const child of el.childNodes) {
     const type = child.nodeType;
     if (type === Node.TEXT_NODE) {
       const text = child.textContent;
-      const newText = text.replaceAll(/{{([^}]+)}}/g, (match) => {
-        const expression = match.substring(2, match.length - 2);
+      // TODO: How can you use a capture group to get only inside the braces?
+      const newText = text.replaceAll(RE, (match) => {
+        const expression = match.substring(1, match.length - 1);
         return evaluate(expression);
       });
       child.nodeValue = newText;
     } else if (type === Node.ELEMENT_NODE) {
-      updateText(child, evaluate);
+      updateText(child, evaluate); // recursive call
     }
   }
 }
 
 document.addEventListener("alpine:init", () => {
   Alpine.directive("interp", (el, {}, { effect, evaluate }) => {
-    effect(() => {
-      updateText(el, evaluate);
-    });
+    effect(() => updateText(el, evaluate));
   });
 });
